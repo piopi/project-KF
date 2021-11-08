3fe9569bbd873b200b521d2315c43f67d20f5247
Mostapha El Sabah <team.piopi@gmail.com>
2021-11-07 19:16:16 -0500

    Ensure Stack run locally and on Heroku

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,14 +1,21 @@
 import { Sequelize } from 'sequelize';
 
-const db: Sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://user:pass@postgres:5432/dashboard', {
-  logging: false,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-    }
-  },
-});
+let options: any = { logging: false };
+if (process.env.TYPE_ENV !== 'Dev') {
+  options = {
+    ...options,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+}
+const db: Sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://user:pass@postgres:5432/dashboard',
+  options,
+);
 
 export default db;
